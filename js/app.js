e43029d0bc2c0ba8c980af52f388cc42e4c35a81
bijan102 <bfazeli1@gmail.com>
2018-03-16 14:31:46 -0700

    Added retrieve facts and retrieve list functionality

--- js/app.js
+++ js/app.js
@@ -3,7 +3,78 @@ let chuck = new Vue ({
 
     data: {
         appName: 'Chuck Facts',
+        isFetching: false,
         randFact: '',
-        factsArray: []
+        choice: '',
+        query: '',
+        categories: ["hi", "bye"]
+    },
+
+    methods: {
+        getCategories () {
+
+            let viewModel = this
+            console.log(this.appName)
+
+            axios.get('https://api.chucknorris.io/jokes/categories', {
+                headers: {
+                    Accept: 'application/json'
+                }
+            })
+            .then((response) => {
+                console.log(response)
+                viewModel.categories = response.data
+                console.log(viewModel.categories)
+                viewModel.randFact = response.data[0]
+                console.log(viewModel.appName)
+            })
+            .catch((err) => {
+                alert(err)
+            })
+                
+        },
+        retrieveFact () {
+            let vm = this
+            console.log(this.appName)
+
+            axios.get(`https://api.chucknorris.io/jokes/random?category=${vm.choice}`, {
+                headers: {
+                    Accept: 'application/json'
+                }
+            })
+            .then((response) => {
+                vm.randFact = response.data.value
+            })
+            .catch((err) => {
+                alert(err)
+            })
+        },
+        retrieveList () {
+            let vm = this
+            console.log(this.appName)
+
+            axios.get(`https://api.chucknorris.io/jokes/search?query=${query}`, {
+                headers: {
+                    Accept: 'application/json'
+                }
+            })
+            .then((response) => {
+                vm.randFact = response.data.value
+            })
+            .catch((err) => {
+                alert(err)
+            })
+        }
+    },
+    filters: {
+        highlight: (phrase) => {
+            if(!phrase) return ''
+            let result
+        }
+    },
+    beforeMount() {
+        this.getCategories()
     }
+
+    
 })
